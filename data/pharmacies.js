a77ffe28259e1bef6c8a96513b98021fa9110033
Drew Bernard <82413471+TeamUpswell@users.noreply.github.com>
2024-12-18 11:02:18 -0800

    changed names and added new data file

---
+++ data/pharmacies.js
@@ -0,0 +1,50 @@
+pharmacies = [
+    {
+        "name": "Hajjo Yawo Pharmacy",
+        "address": "Jimeta, Yola, Adamawa",
+        "latitude": 9.261733993,
+        "longitude": 12.45380743
+    },
+    {
+        "name": "Hakuri Pharmacy Bekaji",
+        "address": "Bekaji Corner, Adjacent Bekaji Primary School",
+        "latitude": 9.2618049,
+        "longitude": 12.4443273
+    },
+    {
+        "name": "Hakuri Pharmacy Jimeta",
+        "address": "80 Mohammed Mustafa Road, Jimeta, Adamawa",
+        "latitude": 9.2797299,
+        "longitude": 12.4584676
+    },
+    {
+        "name": "Shekinah Pharmacy",
+        "address": "Adjacent Central Bank of Nigeria, Opposite First Bank, Bank Road, Jimeta",
+        "latitude": 9.2686101,
+        "longitude": 12.4439691
+    },
+    {
+        "name": "Balavin Pharmacy",
+        "address": "No 2/3 Army Barracks Road, Jimeta",
+        "latitude": 9.2716518,
+        "longitude": 12.4528907
+    },
+    {
+        "name": "Jimeta Central Pharmacy",
+        "address": "148 Mohammed Mustapha Way, Yola",
+        "latitude": 9.2034963,
+        "longitude": 12.4953899
+    },
+    {
+        "name": "Panyi Pharmacy Ltd",
+        "address": "Bajabure Federal Housing Estate, Girei",
+        "latitude": 9.3222588,
+        "longitude": 12.4774567
+    },
+    {
+        "name": "L&L Pharmacy",
+        "address": "KR Paktajoma Plaza, Bachure Road, Jimeta-Yola South, Behind Jezco Oil",
+        "latitude": 9.2425304,
+        "longitude": 12.4196523
+    }
+];
---
+++ data/phcs.js
@@ -0,0 +1,308 @@
+const healthcareCenters = [
+    {
+        "name": "Bajabure Primary Health Clinic",
+        "lga": "Girei",
+        "address": "8F39=42H, Unnamed Road, Jabura Adamawa",
+        "days_of_immunization": "",
+        "hours_of_work": "",
+        "latitude": 9.304502,
+        "longitude": 12.4810542
+    },
+    {
+        "name": "Damare Primary Health Care Center",
+        "lga": "Girei",
+        "address": "6F7Q+GJH, Yola, 640230",
+        "days_of_immunization": "",
+        "hours_of_work": "",
+        "latitude": 9.2138125,
+        "longitude": 12.4891094
+    },
+    {
+        "name": "Sangere Primary Health Clinic",
+        "lga": "Girei",
+        "address": "Adjacent MAUTECH, Girei",
+        "days_of_immunization": "",
+        "hours_of_work": "",
+        "latitude": 9.348933,
+        "longitude": 12.503175
+    },
+    {
+        "name": "Jabbilamba Primary Health Centre",
+        "lga": "Girei",
+        "address": "GH2F+VR, Jiberu 652108",
+        "days_of_immunization": "",
+        "hours_of_work": "",
+        "latitude": 9.5021875,
+        "longitude": 12.5745625
+    },
+    {
+        "name": "Tambo Primary Health Centre",
+        "lga": "Girei",
+        "address": "G8WX+76C, Tambua 652107",
+        "days_of_immunization": "",
+        "hours_of_work": "",
+        "latitude": 9.5456875,
+        "longitude": 12.3480156
+    },
+    {
+        "name": "Vunoklang Primary Health Centre",
+        "lga": "Girei",
+        "address": "7GR5+MV6, Modire 652101",
+        "days_of_immunization": "",
+        "hours_of_work": "",
+        "latitude": 9.2916625,
+        "longitude": 12.5096406
+    },
+    {
+        "name": "Dakri Primary Health Care Center",
+        "lga": "Girei",
+        "address": "Dakri Alhaji, Dakri Ward, Bajabure, LG, District, Girei, Adamawa",
+        "days_of_immunization": "",
+        "hours_of_work": "",
+        "latitude": 9.291738,
+        "longitude": 12.4745142
+    },
+    {
+        "name": "Ngawa Primary Health Centre",
+        "lga": "Girei",
+        "address": "FFV2+8RH, Namtari Kahu 652107",
+        "days_of_immunization": "",
+        "hours_of_work": "",
+        "latitude": 9.4933125,
+        "longitude": 12.4521094
+    },
+    {
+        "name": "Girei B Primary Health Centre",
+        "lga": "Girei",
+        "address": "9H92+56X, Girei 652108",
+        "days_of_immunization": "",
+        "hours_of_work": "",
+        "latitude": 9.3679875,
+        "longitude": 12.5506094
+    },
+    {
+        "name": "Girei A Primary Health Centre",
+        "lga": "Girei",
+        "address": "9G8R+382, Girei 652101",
+        "days_of_immunization": "",
+        "hours_of_work": "",
+        "latitude": 9.3651375,
+        "longitude": 12.5407656
+    },
+    {
+        "name": "Ajiya Primary Health Care Center",
+        "lga": "Yola North",
+        "address": "7FH5+7MP, Jimeta, 640102",
+        "days_of_immunization": "",
+        "hours_of_work": "",
+        "latitude": 9.2782125,
+        "longitude": 12.4592031
+    },
+    {
+        "name": "Bachure Primary Health Care Center",
+        "lga": "Yola North",
+        "address": "6CVF+9Q4, Wuro Jebbe 640101",
+        "days_of_immunization": "",
+        "hours_of_work": "",
+        "latitude": 9.2433875,
+        "longitude": 12.4244531
+    },
+    {
+        "name": "Federal College of Education Clinic",
+        "lga": "Yola North",
+        "address": "Jimeta,Yola Rd, Malamre",
+        "days_of_immunization": "",
+        "hours_of_work": "",
+        "latitude": 9.2451085,
+        "longitude": 12.4555975
+    },
+    {
+        "name": "Malamre PHC",
+        "lga": "Yola North",
+        "address": "Karewa GRA Extension, Jimeta 640101",
+        "days_of_immunization": "",
+        "hours_of_work": "",
+        "latitude": 9.2444243,
+        "longitude": 12.4509552
+    },
+    {
+        "name": "Aso Rock Primary Health Clinic",
+        "lga": "Yola North",
+        "address": "Jambutu, Jimeta 640101, Adamawa",
+        "days_of_immunization": "",
+        "hours_of_work": "",
+        "latitude": 9.2847759,
+        "longitude": 12.4237297
+    },
+    {
+        "name": "Bekaji Primary Health Clinic",
+        "lga": "Yola North",
+        "address": "7C5Q+VQ3, Bekaji Rd, Karewa, Jimeta 640101",
+        "days_of_immunization": "",
+        "hours_of_work": "",
+        "latitude": 9.2596375,
+        "longitude": 12.4394219
+    },
+    {
+        "name": "Damilu Primary Health Clinic",
+        "lga": "Yola North",
+        "address": "Wuro Jabbe, Jimeta 640101, Adamawa",
+        "days_of_immunization": "",
+        "hours_of_work": "",
+        "latitude": 9.2658533,
+        "longitude": 12.4208642
+    },
+    {
+        "name": "Atiku Abubakar Primary Health Care Center",
+        "lga": "Yola North",
+        "address": "Obasanjo Street Jimeta Yola",
+        "days_of_immunization": "",
+        "hours_of_work": "",
+        "latitude": 9.2034963,
+        "longitude": 12.4953899
+    },
+    {
+        "name": "Gambo Jimeta Primary Health Care Center",
+        "lga": "Yola North",
+        "address": "Opposite Custom Office Zone 3 Wuro Jebbe",
+        "days_of_immunization": "",
+        "hours_of_work": "",
+        "latitude": 9.254985,
+        "longitude": 12.4208642
+    },
+    {
+        "name": "Jambutu Primary Health Care Centre",
+        "lga": "Yola North",
+        "address": "7CMH+WV7, Jambutu, Jimeta 640101",
+        "days_of_immunization": "",
+        "hours_of_work": "",
+        "latitude": 9.2847875,
+        "longitude": 12.4296719
+    },
+    {
+        "name": "Major Aminu Primary Health Care Centre",
+        "lga": "Yola North",
+        "address": "Doubeli Yola North Adamawa",
+        "days_of_immunization": "",
+        "hours_of_work": "",
+        "latitude": 9.1737191,
+        "longitude": 12.4151335
+    },
+    {
+        "name": "Nassarawo Maternal And Child Health Clinic",
+        "lga": "Yola North",
+        "address": "7CJV+3GV, Jimeta, 640101",
+        "days_of_immunization": "",
+        "hours_of_work": "",
+        "latitude": 9.2802375,
+        "longitude": 12.4437969
+    },
+    {
+        "name": "Nassarawo Primary Health Care Center",
+        "lga": "Yola North",
+        "address": "7CJV+3GV, Jimeta, 640101",
+        "days_of_immunization": "",
+        "hours_of_work": "",
+        "latitude": 9.2802375,
+        "longitude": 12.4437969
+    },
+    {
+        "name": "Police Medical Center",
+        "lga": "Yola North",
+        "address": "10 Police Roundabout Jimeta Yola",
+        "days_of_immunization": "",
+        "hours_of_work": "",
+        "latitude": 9.258661,
+        "longitude": 12.4565086
+    },
+    {
+        "name": "Peace Hospital",
+        "lga": "Yola North",
+        "address": "2, Luggere Street Jimeta Yola",
+        "days_of_immunization": "",
+        "hours_of_work": "",
+        "latitude": 9.2451085,
+        "longitude": 12.4555975
+    },
+    {
+        "name": "Jemita Maternity Clinic",
+        "lga": "Yola North",
+        "address": "43 Bishop Street, Jimeta, Yola",
+        "days_of_immunization": "",
+        "hours_of_work": "",
+        "latitude": 9.2760113,
+        "longitude": 12.4497796
+    },
+    {
+        "name": "Gwadabawa Primary Health Care Center",
+        "lga": "Yola North",
+        "address": "Gwadabawa Jimeta Yola",
+        "days_of_immunization": "",
+        "hours_of_work": "",
+        "latitude": 9.2716518,
+        "longitude": 12.4528907
+    },
+    {
+        "name": "Bako Primary Health Care Centre",
+        "lga": "Yola South",
+        "address": "6F3F+97V, Yola, 640284",
+        "days_of_immunization": "",
+        "hours_of_work": "",
+        "latitude": 9.2034875,
+        "longitude": 12.4731719
+    },
+    {
+        "name": "Goduwo Primary Health Clinic",
+        "lga": "Yola South",
+        "address": "Ngurore Adamawa",
+        "days_of_immunization": "",
+        "hours_of_work": "",
+        "latitude": 9.2894242,
+        "longitude": 12.2416715
+    },
+    {
+        "name": "Nana Asma'u Primary Health Care Centre",
+        "lga": "Yola South",
+        "address": "Adjacent the palace, Yola town",
+        "days_of_immunization": "",
+        "hours_of_work": "",
+        "latitude": 9.2034963,
+        "longitude": 12.4953899
+    },
+    {
+        "name": "Shagari Primary Health Care Center",
+        "lga": "Yola South",
+        "address": "5FVH+5JX, Madumari, Yola 640284, Adamawa",
+        "days_of_immunization": "",
+        "hours_of_work": "",
+        "latitude": 9.1929875,
+        "longitude": 12.4791094
+    },
+    {
+        "name": "Wauru Jabbe Primary Health Clinic",
+        "lga": "Yola South",
+        "address": "Namtari Wurro Jabbe Adamawa",
+        "days_of_immunization": "",
+        "hours_of_work": "",
+        "latitude": 9.3070992,
+        "longitude": 12.2905625
+    },
+    {
+        "name": "Bakari Mbamoi",
+        "lga": "Yola South",
+        "address": "6F2H+JQJ, Yola, 640284",
+        "days_of_immunization": "",
+        "hours_of_work": "",
+        "latitude": 9.2015875,
+        "longitude": 12.4793906
+    },
+    {
+        "name": "FMC Yola",
+        "lga": "Yola South",
+        "address": "Yola Bye - Pass Rd, Madumari, Yola 640210",
+        "days_of_immunization": "",
+        "hours_of_work": "",
+        "latitude": 9.1957937,
+        "longitude": 12.4819925
+    }
+];
