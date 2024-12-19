7604fb94677fca7a0e7ca5fb57a7f5448bb2c8ed
Drew Bernard <82413471+TeamUpswell@users.noreply.github.com>
2024-12-19 14:57:12 -0800

    syncing with abuja

--- data/pharmacies.js
+++ data/pharmacies.js
@@ -1,48 +1,64 @@
-pharmacies = [
+const pharmacies = [
     {
+        "id": "p1",
         "name": "Hajjo Yawo Pharmacy",
+        "lga": "Girei",
         "address": "Jimeta, Yola, Adamawa",
         "latitude": 9.261733993,
         "longitude": 12.45380743
     },
     {
+        "id": "p2",
         "name": "Hakuri Pharmacy Bekaji",
+        "lga": "Yola North",
         "address": "Bekaji Corner, Adjacent Bekaji Primary School",
         "latitude": 9.2618049,
         "longitude": 12.4443273
     },
     {
+        "id": "p3",
         "name": "Hakuri Pharmacy Jimeta",
+        "lga": "Yola North",
         "address": "80 Mohammed Mustafa Road, Jimeta, Adamawa",
         "latitude": 9.2797299,
         "longitude": 12.4584676
     },
     {
+        "id": "p4",
         "name": "Shekinah Pharmacy",
+        "lga": "Yola North",
         "address": "Adjacent Central Bank of Nigeria, Opposite First Bank, Bank Road, Jimeta",
         "latitude": 9.2686101,
         "longitude": 12.4439691
     },
     {
+        "id": "p5",
         "name": "Balavin Pharmacy",
+        "lga": "Yola North",
         "address": "No 2/3 Army Barracks Road, Jimeta",
         "latitude": 9.2716518,
         "longitude": 12.4528907
     },
     {
+        "id": "p6",
         "name": "Jimeta Central Pharmacy",
+        "lga": "Yola North",
         "address": "148 Mohammed Mustapha Way, Yola",
         "latitude": 9.2034963,
         "longitude": 12.4953899
     },
     {
+        "id": "p7",
         "name": "Panyi Pharmacy Ltd",
+        "lga": "Girei",
         "address": "Bajabure Federal Housing Estate, Girei",
         "latitude": 9.3222588,
         "longitude": 12.4774567
     },
     {
+        "id": "p8",
         "name": "L&L Pharmacy",
+        "lga": "Yola North",
         "address": "KR Paktajoma Plaza, Bachure Road, Jimeta-Yola South, Behind Jezco Oil",
         "latitude": 9.2425304,
         "longitude": 12.4196523
